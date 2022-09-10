22339daa8ac0a56134a4c0877ff4000f07ba7c4a
Yoni-Satat <yoni.satat@gmail.com>
2022-09-10 14:40:26 +0100

    third commit

---
+++ pages/api/hello.js
@@ -0,0 +1,17 @@
+
+// req = HTTP incoming message, res = HTTP server response
+export default function handler(req, res) {
+    res.status(200).json({ text: 'hello' });
+    // const email = req.body.email;
+    // Then save email to your database, etc...
+}
+
+// A Good Use Case: Handling Form Input
+// A good use case for API Routes is handling form input. 
+// For example, you can create a form on your page and have it send a POST request
+//  to your API Route. 
+//  You can then write code to directly save it to your database. 
+//  The API Route code will not be part of your client bundle, 
+//  so you can safely write server-side code.
+
+
